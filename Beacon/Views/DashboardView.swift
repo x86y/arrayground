efd899373adfe92a9d25bb08b16ec3ea4fcf5f8c
x86y <lattice0x@protonmail.com>
2023-07-01 11:07:45 +0400

    feat: make dashboard persistent and async

--- Beacon/Views/DashboardView.swift
+++ Beacon/Views/DashboardView.swift
@@ -5,14 +5,70 @@
 
 import Foundation
 import SwiftUI
+import Charts
 
-struct Card: Identifiable {
+struct DataElement: Identifiable, Comparable {
+    var id = UUID()
+    let key: String
+    let value: Int
+    
+    static func < (lhs: DataElement, rhs: DataElement) -> Bool {
+        return lhs.key < rhs.key
+    }
+    
+    static func == (lhs: DataElement, rhs: DataElement) -> Bool {
+        return lhs.key == rhs.key
+    }
+}
+
+struct Card: Identifiable, Codable {
     let id: UUID
     var snippet: String
 }
 
+func parseData(s: String) -> [String: Int] {
+    if s != "" {
+        let components = s.components(separatedBy: "!")
+        let keysString = components[0].trimmingCharacters(in: CharacterSet(charactersIn: "()\""))
+        let valuesString = components[1]
+        let keys = keysString.components(separatedBy: ";")
+        let values = valuesString.split(separator: " ").map { Int($0)! }
+        assert(keys.count == values.count, "Keys and values count mismatch!")
+        var dict: [String: Int] = [:]
+        for (index, key) in keys.enumerated() {
+            dict[key] = values[index]
+        }
+        return dict
+    } else {
+        return [:]
+    }
+}
+
+class DashboardViewModel: ObservableObject {
+    @Published var cards: [Card] = [] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(cards) {
+                UserDefaults.standard.set(encoded, forKey: "cards")
+            }
+        }
+    }
+    
+    
+    init() {
+        if let cardsData = UserDefaults.standard.data(forKey: "cards") {
+            let decoder = JSONDecoder()
+            if let loadedCards = try? decoder.decode([Card].self, from: cardsData) {
+                self.cards = loadedCards
+                return
+            }
+        }
+        self.cards = [Card(id: UUID(), snippet: "")]
+    }
+}
+
 struct Dashboard: View {
-    @State private var cards: [Card] = [Card(id: UUID(), snippet: "")]
+    @StateObject var viewModel = DashboardViewModel()
     var body: some View {
         VStack {
             Text("Dashboard")
@@ -22,21 +78,21 @@ struct Dashboard: View {
             Spacer()
             ScrollView(.vertical, showsIndicators: false) {
                 VStack {
-                    ForEach(0 ..< cards.count, id: \.self) { index in
-                        CardView(card: $cards[index], removeCard: {
-                            cards.remove(at: index)
+                    ForEach(0 ..< viewModel.cards.count, id: \.self) { index in
+                        CardView(card: $viewModel.cards[index], removeCard: {
+                            viewModel.cards.remove(at: index)
                         })
                         .padding(.horizontal)
                     }
                     
                     Button(action: {
-                        self.cards.append(Card(id: UUID(), snippet: ""))
+                        viewModel.cards.append(Card(id: UUID(), snippet: ""))
                     }) {
                         HStack { Image(systemName: "plus") }
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
                     }
                     .padding(.horizontal)
                 }
@@ -50,8 +106,12 @@ struct Dashboard: View {
 struct CardView: View {
     @Binding var card: Card
     let removeCard: () -> Void
+    @State private var barSelection: String?
     @State private var tempSnippet: String = ""
+    @State private var isLoading: Bool = false
+    @State private var isEditing: Bool = false
     @StateObject var updater: Updater = Updater()
+    var data: [String: Int] { return parseData(s: updater.output) }
     
     var body: some View {
         VStack{
@@ -59,15 +119,13 @@ struct CardView: View {
                 VStack {
                     HStack {
                         Spacer()
-                        Button(action: {
-                            self.removeCard()
-                        }) {
+                        Button(action: { self.removeCard() }) {
                             Image(systemName: "xmark.circle.fill")
                                 .font(.title2)
                                 .foregroundColor(.red)
                         }
                         Button(action: {
-                            self.card.snippet = ""
+                            self.isEditing = true
                             updater.deinit_timer()
                         }) {
                             Image(systemName: "pencil.circle.fill")
@@ -76,13 +134,14 @@ struct CardView: View {
                         }
                         .padding(.trailing)
                     }
-                    if self.card.snippet.isEmpty {
+                    if self.isEditing {
                         TextEditor(text: $tempSnippet)
                             .padding(.horizontal)
                             .navigationTitle("snippet")
                             .onChange(of: tempSnippet) {
                                 if tempSnippet.suffix(2) == "\n\n" {
                                     self.card.snippet = self.tempSnippet
+                                    self.isEditing = false
                                     self.updater.snippet = self.card.snippet
                                     updater.init_timer()
                                 }
@@ -94,39 +153,127 @@ struct CardView: View {
                             .border(.yellow)
                             .padding()
                     } else {
-                        Text(updater.output)
-                            .frame(width: 350, height: 350)
-                            .border(.yellow)
-                            .font(.body)
-                            .padding()
+                        ZStack {
+                            if updater.isLoading {
+                                ProgressView().frame(width: 350, height: 350)
+                            }
+                            VStack {
+                                VStack {
+                                    if !updater.output.contains("Error") && updater.output.contains("!") { // FIXME need a better check if the output is a dict
+                                        Chart {
+                                            ForEach(data.map { DataElement(key: $0.key, value: $0.value) }.sorted()) { item in
+                                                BarMark(
+                                                    x: .value("Key", item.key),
+                                                    y: .value("Count", item.value)
+                                                )
+                                                .cornerRadius(8)
+                                                .foregroundStyle(by: .value("Result Color", item.key))
+                                            }
+                                            if let barSelection {
+                                                RuleMark(x: .value("Key", barSelection))
+                                                    .foregroundStyle(.gray.opacity(0.35))
+                                                    .zIndex(-10)
+                                                    .offset(yStart: -15)
+                                                    .annotation(
+                                                        position: .top,
+                                                        spacing: 0,
+                                                        overflowResolution: .init(x: .disabled, y: .disabled)) {
+                                                            ChartPopOverView(xval: barSelection, yval: data[barSelection] ?? 0)
+                                                        }
+                                            }
+                                        }
+                                        .chartXSelection(value: $barSelection)
+                                        .chartLegend(position: .bottom, alignment: .leading, spacing: 25)
+                                        .padding(.top, 15)
+                                        .chartYAxis {
+                                            AxisMarks(position: .leading) { _ in
+                                                AxisValueLabel()
+                                            }
+                                        }
+                                        .chartXAxis {
+                                            AxisMarks(position: .bottom) { _ in
+                                                AxisValueLabel()
+                                            }
+                                        }
+                                    } else {
+                                        Text(updater.output)
+                                            .font(.body)
+                                            .padding()
+                                        Spacer()
+                                    }
+                                    /*
+                                    ProgressView(value: updater.timeRemaining, total: updater.refreshRate)
+                                        .accentColor(.green)
+                                        .frame(height: 8.0)
+                                        .scaleEffect(x: 1, y: 2, anchor: .center)
+                                     */
+                                }
+                                .frame(width: 350, height: 350)
+                            }
+                        }
+                        .border(.blue)
                     }
                 }
                 .padding()
             }
+        }.onAppear(perform: {
+            if !card.snippet.isEmpty {
+                updater.snippet = card.snippet
+                updater.init_timer()
+            }
+        })
+    }
+    @ViewBuilder
+    func ChartPopOverView(xval: String, yval: Int) -> some View {
+        VStack(alignment: .leading, spacing: 6) {
+            HStack(spacing: 4) {
+                Text(String(yval))
+                    .font(.title3)
+                Text(xval)
+                    .font(.title3)
+                    .textScale(.secondary)
+            }
         }
+        .padding()
+        .background(.cyan, in: .rect(cornerRadius: 8))
     }
 }
 
+
 class Updater: ObservableObject {
     var snippet: String = ""
+    var refreshRate: Double = 15.0
+    @Published var timeRemaining: Double = 0.0
     @Published var output: String = ""
+    @Published var isLoading: Bool = false
     var timer: Timer?
     
-   deinit {
+    deinit {
         timer?.invalidate()
     }
     
     func init_timer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 15, repeats: true, block: { _ in
-            self.refresh()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+            if (self.timeRemaining == self.refreshRate) {
+                Task {
+                    await self.refresh()
+                }
+                self.timeRemaining = 0.0
+            } else {
+                self.timeRemaining += 1.0
+            }
         })
-        self.refresh()
+        Task {
+            await self.refresh()
+        }
     }
-   func deinit_timer() {
+    
+    func deinit_timer() {
         timer?.invalidate()
     }
     
-    func refresh() {
+    func refresh() async {
+        isLoading = true
         let snippets = self.snippet.split(separator: "\n")
         var to: String
         for snippet in snippets {
@@ -135,5 +282,7 @@ class Updater: ObservableObject {
             : ke(input: String(snippet))
             if snippet == snippets.last { output = to }
         }
+        isLoading = false
     }
 }
+
